0973782b1a70edb21ba6aa4007c7a6dd50a66693
Jibril Ahmed <jebrildomenico@gmail.com>
2026-02-23 06:33:19 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="OpenTI",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # add dependencies here
+    ],
+    python_requires='>=3.11',
+)
